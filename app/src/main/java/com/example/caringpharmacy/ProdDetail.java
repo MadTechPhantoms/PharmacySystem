5ad749c3323975f81678ed87812523b77dd0f80a
Thaanish Ahamed <89010139+IT20648190@users.noreply.github.com>
2022-05-01 21:02:11 +0530

    Second commit(products)

---
+++ app/src/main/java/com/example/caringpharmacy/ProdDetail.java
@@ -0,0 +1,14 @@
+package com.example.caringpharmacy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProdDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prod_detail);
+    }
+}
---
+++ app/src/main/java/com/example/caringpharmacy/ProdViewCus.java
@@ -0,0 +1,14 @@
+package com.example.caringpharmacy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProdViewCus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prod_view_cus);
+    }
+}
