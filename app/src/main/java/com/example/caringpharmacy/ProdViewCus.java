178603df70f9bf32dcbaa57106aca950ccf3a92a
Thaanish Ahamed <89010139+IT20648190@users.noreply.github.com>
2022-05-08 16:55:43 +0530

    CRD commit(products)

--- app/src/main/java/com/example/caringpharmacy/ProdViewCus.java
+++ app/src/main/java/com/example/caringpharmacy/ProdViewCus.java
@@ -1,14 +1,47 @@
 package com.example.caringpharmacy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ProdViewCus extends AppCompatActivity {
+
+    RecyclerView recview;
+    prodadapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prod_view_cus);
+
+        recview = (RecyclerView) findViewById(R.id.recview);
+        recview.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<modelprod> options =
+                new FirebaseRecyclerOptions.Builder<modelprod>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Product"), modelprod.class)
+                        .build();
+
+        adapter = new prodadapter(options);
+        recview.setAdapter(adapter);
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
 }
