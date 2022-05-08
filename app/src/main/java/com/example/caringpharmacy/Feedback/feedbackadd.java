4c336faca46f22b9070f787989fd65e127baeb3c
Dilini98 <86178588+IT20658472@users.noreply.github.com>
2022-05-08 10:22:17 +0530

    crud(add to feedback)

--- app/src/main/java/com/example/caringpharmacy/Feedback/feedbackadd.java
+++
@@ -1,4 +0,0 @@
-package com.example.caringpharmacy.Feedback;
-
-public class feedbackadd {
-}
--- app/src/main/java/com/example/caringpharmacy/Fragment/FeedbackFragment.java
+++ app/src/main/java/com/example/caringpharmacy/Fragment/FeedbackFragment.java
@@ -24,7 +24,6 @@ public class FeedbackFragment extends Fragment {
     public FeedbackFragment() {
         // Required empty public constructor
     }
-    //code
 
 
     @Override
